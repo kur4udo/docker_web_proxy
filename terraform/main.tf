1f0ea090f5953d1f2fcf37a15a0537d751537c69
Tomasz <ec2-user@ip-172-31-40-227.eu-west-1.compute.internal>
2019-11-03 11:47:08 +0000

    add:EC2 key and user data, fix: docker_compose.yml

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,6 +15,10 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
+data "template_file" "server_init" {
+  template = "${file("server_init.tpl")}"
+}
+
 ## VPC
 
 resource "aws_default_vpc" "default_vpc" {
@@ -32,20 +36,12 @@ resource "aws_security_group" "sg" {
 
   ingress {
 
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-
-    from_port   = 8081
-    to_port     = 8081
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  
   ingress {
 
     from_port   = 9000
@@ -60,18 +56,31 @@ resource "aws_security_group" "sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    Name = "Docker HAProxy Spring Boot Web Security Group"
+  }
 }
 
 ## EC2
 
 resource "aws_instance" "ubuntu_server" {
-  #ami           = "${data.aws_ami.ubuntu.id}"
-  ami           = "ami-02df9ea15c1778c9c"
+  ami           = "${data.aws_ami.ubuntu.id}"
   instance_type = "${var.instance_type}"
 
   vpc_security_group_ids = ["${aws_security_group.sg.id}"]
 
+  key_name = "${aws_key_pair.ec2_key.id}"
+
+  user_data = "${base64encode(data.template_file.server_init.rendered)}"
+
   tags = {
-    Name = "Docker HAProxy Spring Boot Web"
+    Name = "Docker HAProxy Spring Boot Web EC2"
   }
+}
+
+## Public key
+
+resource "aws_key_pair" "ec2_key" {
+  public_key = "${var.public_key}"
 }
